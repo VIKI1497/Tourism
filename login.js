27458a85cc946f804fb0a2d5a549ca0ce81f254a
VIKI1497 <vigneshagnimuthu@gmail.com>
2023-03-08 16:16:39 +0530

    Login model

---
+++ login.js
@@ -0,0 +1,8 @@
+var modal = document.getElementById("log1");
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};

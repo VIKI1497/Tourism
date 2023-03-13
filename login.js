bf22a1397c7c4bb6a5da39d68151d3846b5e966e
VIKI1497 <vigneshagnimuthu@gmail.com>
2023-03-13 14:00:53 +0530

    Weather Api Feature

--- login.js
+++ login.js
@@ -6,3 +6,40 @@ window.onclick = function (event) {
     modal.style.display = "none";
   }
 };
+
+/* Weather API */
+function getWeather() {
+  var location = document.getElementById("location").value;
+  var apiKey = "083e964db844fec2e265044f925dfb54";
+  var url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    location +
+    "&appid=" +
+    apiKey;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      var city = data.name;
+      var temp = Math.round(data.main.temp - 273.15);
+      var desc = data.weather[0].description;
+      var country = data.sys.country;
+      var icon =
+        "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+      document.getElementById("weather").innerHTML =
+        "<img src='" +
+        icon +
+        "' alt='Weather Icon'>   " +
+        city +
+        "  |" +
+        temp +
+        "°C |" +
+        desc +
+        " |" +
+        "country : " +
+        country;
+
+      console.log(data);
+    })
+    .catch((error) => alert("Enter the valid city!!"));
+}
